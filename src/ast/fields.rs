104275c6772226058ead02602d3c7935d2434b5c
Cameron Eldridge <cameldridge@gmail.com>
2020-11-02 00:19:28 -0500

    Change the shape of the fields rule

--- src/ast/fields.rs
+++ src/ast/fields.rs
@@ -3,30 +3,35 @@ use crate::parser::Rule;
 
 pub(crate) fn fields(pair: crate::Pair, context: &mut Context) -> (Arity, Vec<Pattern>) {
     assert_eq!(pair.as_rule(), Rule::fields);
-    pair.into_inner().map(|pair| field(pair, context)).fold(
-        (Arity::default(), vec![]),
-        |(mut arity, mut patterns), (name, pattern)| {
-            match name {
-                Some(name) => arity.push(name, 1),
-                None => arity.extend_len(),
-            }
-            patterns.push(pattern);
-            (arity, patterns)
-        },
-    )
-}
-
-fn field(pair: crate::Pair, context: &mut Context) -> (Option<Atom>, Pattern) {
-    assert_eq!(pair.as_rule(), Rule::field);
-    let pair = just!(pair.into_inner());
-    match pair.as_rule() {
-        Rule::named_field => {
-            let mut pairs = pair.into_inner();
-            let atom = Atom::new(pairs.next().unwrap());
-            let pattern = Pattern::new(pairs.next().unwrap(), context);
-            (Some(atom), pattern)
-        }
-        Rule::bare_field => (None, Pattern::new(just!(pair.into_inner()), context)),
-        _ => unreachable!(),
+    let mut pairs = pair.into_inner().peekable();
+    let mut arity = Arity::default();
+    let mut patterns = vec![];
+    eprintln!("{:#?}", pairs);
+    if pairs.peek().unwrap().as_rule() == Rule::bare_fields {
+        patterns.extend(
+            pairs
+                .next()
+                .unwrap()
+                .into_inner()
+                .map(|pair| Pattern::new(pair, context)),
+        );
+        arity.len = patterns.len() as u32;
     }
+    match pairs.next() {
+        Some(pair) => {
+            assert_eq!(pair.as_rule(), Rule::named_fields);
+            for pair in pair.into_inner() {
+                let mut pairs = pair.into_inner();
+                let name = Atom::new(pairs.next().unwrap());
+                let values = just!(Rule::bare_fields, pairs)
+                    .into_inner()
+                    .map(|pair| Pattern::new(pair, context))
+                    .collect::<Vec<_>>();
+                arity.push(name, values.len() as u32);
+                patterns.extend(values);
+            }
+        }
+        None => {}
+    }
+    (arity, patterns)
 }
--- src/parser/test/call.rs
+++ src/parser/test/call.rs
@@ -5,6 +5,7 @@ yes!(call_empty_scoped, Rule::call, "hello::test!");
 yes!(call_patterns, Rule::call, "test!(a, b)");
 yes!(call_patterns_scoped, Rule::call, "hello::test!(a, b)");
 yes!(call_named_fields, Rule::call, "test!(left: a, right: b)");
+yes!(call_mixed_fields, Rule::call, "test!(a, left: b, right: c)");
 yes!(call_fancy_scope, Rule::call, "^::^::test!");
 no!(call_no_args, Rule::call, "test!()");
 no!(call_no_excl, Rule::call, "test(3, 4)");
