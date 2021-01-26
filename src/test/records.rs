87d1752016b07668f121b0149568a3ae2ec4aa2a
Cameron Eldridge <cameldridge@gmail.com>
2021-01-25 23:04:20 -0500

    Add a test I thought would fail

--- src/test/records.rs
+++ src/test/records.rs
@@ -9,3 +9,22 @@ test! {
         B = record! { "b" => Value::atom("b") },
         C = record! { "c" => Value::atom("c"), "b" => Value::atom("b") };
 }
+
+test! {
+    record_in_params => "
+    :- pub(recordTest/3).
+    :- use(@core).
+
+    recordTest(
+        T,
+        P,
+        test { p: P, ..Ts },
+    ) :-
+        T =:= test { p: A, q: B, .. },
+        A < B,
+        T =:= test { p: _, ..Ts }.
+    "
+    ?- "recordTest(test { p: 3, q: 4 }, 5, test { p: 5, q: 4 })";
+    ?- "recordTest(test { p: 3, q: 4 }, 5, test(O))"
+        O = record! { "p" => 5, "q" => 4 };
+}
