e563ab6454dffa5bf7ddf9b3292804c62435e3de
Cameron Eldridge <cameldridge@gmail.com>
2021-01-30 18:44:41 -0500

    Remove more unused code

--- src/ast/fields.rs
+++ src/ast/fields.rs
@@ -1,7 +1,6 @@
 use super::*;
 use crate::parser::Rule;
 use std::collections::BTreeMap;
-use std::iter::FromIterator;
 
 #[derive(Clone, Hash, Eq, PartialEq, Default, Debug)]
 pub(crate) struct Fields {
@@ -25,40 +24,5 @@ impl Fields {
 
     pub fn iter(&self) -> impl Iterator<Item = (&Atom, &Pattern)> {
         self.fields.iter()
-    }
-}
-
-impl Into<BTreeMap<Atom, Pattern>> for Fields {
-    fn into(self) -> BTreeMap<Atom, Pattern> {
-        self.fields
-    }
-}
-
-impl From<BTreeMap<Atom, Pattern>> for Fields {
-    fn from(fields: BTreeMap<Atom, Pattern>) -> Self {
-        Self { fields }
-    }
-}
-
-impl<T> FromIterator<T> for Fields
-where
-    BTreeMap<Atom, Pattern>: FromIterator<T>,
-{
-    fn from_iter<I>(iter: I) -> Self
-    where
-        I: IntoIterator<Item = T>,
-    {
-        Fields {
-            fields: BTreeMap::from_iter(iter),
-        }
-    }
-}
-
-impl IntoIterator for Fields {
-    type Item = <BTreeMap<Atom, Pattern> as IntoIterator>::Item;
-    type IntoIter = <BTreeMap<Atom, Pattern> as IntoIterator>::IntoIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.fields.into_iter()
     }
 }
